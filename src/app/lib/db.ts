d1fa1d7ac80cfdc09c2e85efb7c2e16de9d221d2
Michael Ngeno <ngenomichael60@gmail.com>
2024-11-23 13:13:07 +0300

    changes

--- src/app/lib/db.ts
+++ src/app/lib/db.ts
@@ -1,10 +1,10 @@
 import mongoose from "mongoose";
 
-const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017/house-scout";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017/house-scout";
 
 export async function connectToDatabase() {
   if (mongoose.connection.readyState === 0) {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(MONGODB_URI);
     console.log("Connected to MongoDB");
   }
 }
